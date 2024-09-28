18c288367b05980a98d21a4b3550abef4d0e3183
jai <jai@pearlorganisation.com>
2024-09-28 13:47:29 +0530

    updatse

--- src/controller/titles.js
+++ src/controller/titles.js
@@ -1,18 +1,11 @@
 import { asyncHandler } from "../utils/errorHandler/asyncHandler.js";
 import { toursModel } from "../model/tours.js";
 import { treksModel } from "../model/treks.js";
-
+import { specialTripsModel } from "../model/specialTrips.js";
 
 export const getTitles = asyncHandler(async (req, res) => {
-    
-    const tours = await toursModel
-      .find()
-      .select("title")
-    const treks = await treksModel
-      .find()
-      .select("title")
-
-    res.status(200).json({ status: true, treks, tours });
-  });
-  
-  
+  const tours = await toursModel.find().select("title");
+  const treks = await treksModel.find().select("title");
+  const specialTrips = await specialTripsModel.find().select("title");
+  res.status(200).json({ status: true, treks, tours, specialTrips });
+});
